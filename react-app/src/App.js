272fbf784457455a3a2239b8e78ab287c4114ab5
Jason Foo <jason.foo@seekasia.com>
2017-04-21 16:39:16 +0800

    Done the class 3

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,8 +5,21 @@ class App extends React.Component {
         super();
         this.state = {
             txt: 'this is the state text',
-            cat: 0
-        }
+            cat: 0,
+            currentEvent: '---',
+            a: ''
+        };
+        this.updateMe = this.updateMe.bind(this)
+
+    }
+    updateMeAlone(){
+        this.setState({
+            a: this.a.refs.input.value,
+            b: this.refs.b.value
+        })
+    }
+    updateMe (e){
+        this.setState({currentEvent: e.type})
     }
     updateTxt (e){
         this.setState({txt: e.target.value})
@@ -19,6 +32,8 @@ class App extends React.Component {
         cat: React.PropTypes.number.isRequired
     };
 
+
+
     static defaultProps = {
         txt: "dsadsad"
     };
@@ -28,12 +43,43 @@ class App extends React.Component {
             <div>
                 <Widget update={this.updateTxt.bind(this)} />
                 <Widget update={this.updateCat.bind(this)} />
+                <Title text="123453213" />
                 <h1>{this.state.txt} - {this.state.cat}</h1>
                 <h1>hello</h1>
                 <b>{txt}</b>
                 <Button>I <Heart /> React </Button>
+                <textarea onKeyPress={this.updateMe}
+                          onCopy={this.updateMe}
+                          onCut={this.updateMe}
+                          onPaste={this.updateMe}
+                          onFocus={this.updateMe}
+                          onBlur={this.updateMe}
+                          onDoubleClick={this.updateMe}
+                          onTouchStart={this.updateMe}
+                          onTouchMove={this.updateMe}
+                          onTouchEnd={this.updateMe}
+                          rows="10"
+                />
+                <h1>{this.state.currentEvent}</h1>
+                <hr />
+                <InputUpdateMeAlone
+                    ref={ component => this.a = component }
+                    update={this.updateMeAlone.bind(this)}
+                /> {this.state.a}
+                <hr/>
+                <input
+                    ref="b"
+                    type="text"
+                    onChange={this.updateMeAlone.bind(this)}
+                /> {this.state.b}
             </div>
         )
+    }
+}
+
+class InputUpdateMeAlone extends React.Component {
+    render() {
+        return <div><input ref="input" type="text" onChange={this.props.update} /></div>
     }
 }
 
@@ -42,6 +88,20 @@ class Heart extends React.Component {
         return <span>&hearts;</span>
     }
 }
+
+const Title = (props) => <h1>Title: {props.text}</h1>
+
+Title.propTypes = {
+    text(props, propName, component) {
+        if(!(propName in props)){
+            return new Error(`missing ${propName}`);
+        }
+        if(props[propName].length < 6){
+            return new Error(`${propName} was too short`);
+        }
+        console.log(component)
+    }
+};
 
 const Button = (props) =>
     <button>{props.children}</button>
